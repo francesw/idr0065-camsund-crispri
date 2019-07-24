c9c51229d998857dd3660c24997e44de078941e3
Sebastien Besson <seb.besson@gmail.com>
2019-07-24 15:34:28 +0100

    Refactor generate_companions script to be standalone

--- scripts/generate_companions.py
+++ scripts/generate_companions.py
@@ -4,7 +4,7 @@
 # Generate companion files for experimentA of the idr0064 study. This script
 # assumes the following layout for the original data:
 #
-#  <microscopic_slice>/        All data associated with a microscopic slide
+#  <microscopic_slide>/        All data associated with a microscopic slide
 #    pheno/                    Phenotypic acquisition
 #      metadata.txt            Metadata for the phenotyping acquisition
 #      Pos101/                 Position on a microscopyic slide
@@ -18,78 +18,44 @@
 #        4_fam_flour/          Fluorescence acquisition (11 cycles)
 #        phase/                Phase acquisition (11 cycles)
 
+import argparse
 import json
 import logging
-import datetime
+
 from ome_model.experimental import Image, create_companion
 import os
-from os.path import basename, dirname, join, abspath
-import sys
+from os.path import basename, join
 import subprocess
 
 DEBUG = int(os.environ.get("DEBUG", logging.INFO))
-EXPERIMENT_DIRECTORY = join(
-    dirname(abspath(dirname(sys.argv[0]))), 'experimentA')
-METADATA_DIRECTORY = join(EXPERIMENT_DIRECTORY, 'companions')
-BASE_DIRECTORY = join(METADATA_DIRECTORY, 'subpool-1_run-1_EXP-19-BQ3550')
-
-FILEPATHS_TSV = join(EXPERIMENT_DIRECTORY, 'idr0064-experimentA-filePaths.tsv')
-
-PHENO_DIRECTORY = join(BASE_DIRECTORY, 'pheno')
-GENO_DIRECTORY = join(BASE_DIRECTORY, 'geno')
-PHENO_CYCLES = 11
-
 logger = logging.basicConfig(level=DEBUG)
 
 
-# Positions metadata parsing
-metadata = {}
-with open(join(PHENO_DIRECTORY, 'metadata.txt')) as f:
-    metadata_lines = f.readlines()
+def read_phenotypic_positions(directory):
+    """Parse positions of timelapse phenotypic data"""
+    with open(join(directory, 'metadata.txt')) as f:
+        metadata_lines = f.readlines()
 
-positions = {}
-for line in metadata_lines:
-    planes = {}
-    d = json.loads(line)
-    filename_split = d[u'filename'].split("\\")
-    timepoint = int(filename_split[-1][-10:-5])
-    channel = (0 if filename_split[-2] == 'phase' else 1)
-    plane = {(channel, timepoint): {
-        'filename': "%s/%s" % (filename_split[-2], filename_split[-1]),
-        'exposure_time': d['exposure_time'],
-        'timestamp': d['acquire_time'],
+    positions = {}
+    for line in metadata_lines:
+        d = json.loads(line)
+        filename_split = d[u'filename'].split("\\")
+        timepoint = int(filename_split[-1][-10:-5])
+        channel = (0 if filename_split[-2] == 'phase' else 1)
+        plane = {(channel, timepoint): {
+            'filename': "%s/%s" % (filename_split[-2], filename_split[-1]),
+            'exposure_time': d['exposure_time'],
+            'timestamp': d['acquire_time'],
+            }
         }
-    }
-    if d[u'position'] in positions:
-        positions[d[u'position']].update(plane)
-    else:
-        positions[d[u'position']] = plane
+        if d[u'position'] in positions:
+            positions[d[u'position']].update(plane)
+        else:
+            positions[d[u'position']] = plane
+    return positions
 
-# Genotypic metadata generation
-pheno_folders = [join(PHENO_DIRECTORY, x) for x in os.listdir(PHENO_DIRECTORY)]
-pheno_folders = sorted(filter(os.path.isdir, pheno_folders))
-logging.info(
-    "Found %g folders under %s" % (len(pheno_folders), PHENO_DIRECTORY))
 
-for folder in pheno_folders:
-    image = Image(
-        basename(folder), 2048, 879, 1, 2, 481,
-        order="XYZCT", type="uint16")
-    image.add_channel("phase", 0)
-    image.add_channel("fluor", 0)
-
-    planes = positions[basename(folder)]
-    acquisition_date = datetime.datetime.utcfromtimestamp(
-        planes[(0, 0)]['timestamp']/1000.0).isoformat()
-    # print planes
-    for t in range(481):
-        image.add_tiff(planes[(0, t)]["filename"], c=0, z=0, t=t)
-
-    for t in range(240):
-        image.add_tiff(planes[(1, t)]["filename"], c=1, z=0, t=2 * t)
-        image.add_tiff(planes[(1, t)]["filename"], c=1, z=0, t=2 * t + 1)
-    image.add_tiff(planes[(1, 240)]["filename"], c=1, z=0, t=480)
-
+def write_companion(image, folder):
     # Generate companion OME-XML file
     companion_file = join(
         folder, basename(folder) + '.companion.ome')
@@ -103,40 +69,82 @@ for folder in pheno_folders:
     (output, error_output) = proc.communicate()
     logging.info("Created %s" % companion_file)
 
-# Genotypic metadata generation
-geno_folders = [join(GENO_DIRECTORY, x) for x in os.listdir(GENO_DIRECTORY)]
-geno_folders = sorted(filter(os.path.isdir, geno_folders))
-logging.info(
-    "Found %g folders under %s" % (len(geno_folders), GENO_DIRECTORY))
 
-for folder in geno_folders:
-    image = Image(
-        basename(folder), 2048, 879, 1, 5 * PHENO_CYCLES, 1,
-        order="XYZCT", type="uint16")
+def create_phenotypic_companions(directory):
+    # Genotypic metadata generation
+    pheno_directory = join(directory, 'pheno')
+    pheno_folders = [
+        join(pheno_directory, x) for x in os.listdir(pheno_directory)]
+    pheno_folders = sorted(filter(os.path.isdir, pheno_folders))
+    logging.info(
+        "Found %g folders under %s" % (len(pheno_folders), pheno_directory))
 
-    for i in range(PHENO_CYCLES):
-        image.add_channel("phase", 0)
-        image.add_channel("cy5", 0)
-        image.add_channel("cy3", 0)
-        image.add_channel("TxR", 0)
-        image.add_channel("fam", 0)
+    positions = read_phenotypic_positions(pheno_directory)
+    for folder in pheno_folders:
+        image = Image(
+            basename(folder), 2048, 879, 1, 2, 481,
+            order="XYZCT", type="uint16")
+        image.add_channel("phase", -1)
+        image.add_channel("fluor", 16711935)
 
-        index = str(i + 1).zfill(2)
-        image.add_tiff("phase/%s.tiff" % index, c=5 * i, z=0, t=0)
-        image.add_tiff("1_cy5_fluor/%s.tiff" % index, c=5 * i + 1, z=0, t=0)
-        image.add_tiff("2_cy3_fluor/%s.tiff" % index, c=5 * i + 2, z=0, t=0)
-        image.add_tiff("3_TxR_fluor/%s.tiff" % index, c=5 * i + 3, z=0, t=0)
-        image.add_tiff("4_fam_flour/%s.tiff" % index, c=5 * i + 4, z=0, t=0)
+        planes = positions[basename(folder)]
+        # import datetime
+        # acquisition_date = datetime.datetime.utcfromtimestamp(
+        #     planes[(0, 0)]['timestamp']/1000.0).isoformat()
 
-    # Generate companion OME-XML file
-    companion_file = join(
-        folder, basename(folder) + '.companion.ome')
-    create_companion(images=[image], out=companion_file)
+        for t in range(481):
+            image.add_tiff(planes[(0, t)]["filename"], c=0, z=0, t=t)
 
-    # Indent XML for readability
-    proc = subprocess.Popen(
-        ['xmllint', '--format', '-o', companion_file, companion_file],
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE)
-    (output, error_output) = proc.communicate()
-    logging.info("Created %s" % companion_file)
+        for t in range(240):
+            image.add_tiff(planes[(1, t)]["filename"], c=1, z=0, t=2 * t)
+            image.add_tiff(planes[(1, t)]["filename"], c=1, z=0, t=2 * t + 1)
+        image.add_tiff(planes[(1, 240)]["filename"], c=1, z=0, t=480)
+
+        write_companion(image, folder)
+
+
+def create_genotypic_companions(directory):
+    GENO_CYCLES = 11
+
+    # Genotypic metadata generation
+    geno_directory = join(directory, 'geno')
+    geno_folders = [
+        join(geno_directory, x) for x in os.listdir(geno_directory)]
+    geno_folders = sorted(filter(os.path.isdir, geno_folders))
+    logging.info(
+        "Found %g folders under %s" % (len(geno_folders), geno_directory))
+
+    for folder in geno_folders:
+        image = Image(
+            basename(folder), 2048, 879, 1, 5 * GENO_CYCLES, 1,
+            order="XYZCT", type="uint16")
+
+        for i in range(GENO_CYCLES):
+            image.add_channel("phase", -1)
+            image.add_channel("cy5", 0)
+            image.add_channel("cy3", 0)
+            image.add_channel("TxR", 0)
+            image.add_channel("fam", 0)
+
+            index = str(i + 1).zfill(2)
+            image.add_tiff("phase/%s.tiff" % index, c=5 * i, z=0, t=0)
+            image.add_tiff(
+                "1_cy5_fluor/%s.tiff" % index, c=5 * i + 1, z=0, t=0)
+            image.add_tiff(
+                "2_cy3_fluor/%s.tiff" % index, c=5 * i + 2, z=0, t=0)
+            image.add_tiff(
+                "3_TxR_fluor/%s.tiff" % index, c=5 * i + 3, z=0, t=0)
+            image.add_tiff(
+                "4_fam_flour/%s.tiff" % index, c=5 * i + 4, z=0, t=0)
+
+        write_companion(image, folder)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'path', type=str, help='Path to the microscopic slide folder')
+    args = parser.parse_args()
+
+    create_phenotypic_companions(args.path)
+    create_genotypic_companions(args.path)
