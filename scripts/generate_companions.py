794fc4c250bd1808ba57dee7d257fde102b7f7bf
Sebastien Besson <seb.besson@gmail.com>
2019-10-22 11:51:06 +0100

    Refactor generate_companions for the complete submission
    - use metadata files to include timestamps and exposure times
    - generate sparse OME-TIFF filesets
    - create multi-position companion files

--- scripts/generate_companions.py
+++ scripts/generate_companions.py
@@ -18,46 +18,85 @@
 #        4_fam_flour/          Fluorescence acquisition (11 cycles)
 #        phase/                Phase acquisition (11 cycles)
 
-import argparse
-import json
 import logging
 
 from ome_model.experimental import Image, create_companion
 import os
-from os.path import basename, join
+from os.path import basename, join, dirname, abspath
 import subprocess
+import sys
+
+BASE_DIRECTORY = "/uod/idr/filesets/idr0065-camsund-crispri/"
+TIMESTAMPS_FILE = join(BASE_DIRECTORY, "20190823-ftp", "acq_time.txt")
+MAPPINGS_FILE = join(BASE_DIRECTORY, "20190823-ftp", "fluor_phase_link.txt")
+EXPERIMENTA_DIRECTORY = join(
+    dirname(abspath(dirname(sys.argv[0]))), 'experimentA', 'companions')
+EXPERIMENTB_DIRECTORY = join(
+    dirname(abspath(dirname(sys.argv[0]))), 'experimentB', 'companion')
 
 DEBUG = int(os.environ.get("DEBUG", logging.INFO))
 logger = logging.basicConfig(level=DEBUG)
 
 
-def read_phenotypic_positions(directory):
-    """Parse positions of timelapse phenotypic data"""
-    with open(join(directory, 'metadata.txt')) as f:
-        metadata_lines = f.readlines()
-
-    positions = {}
-    for line in metadata_lines:
-        d = json.loads(line)
-        filename_split = d[u'filename'].split("\\")
-        timepoint = int(filename_split[-1][-10:-5])
-        channel = (0 if filename_split[-2] == 'phase' else 1)
-        plane = {(channel, timepoint): {
-            'filename': "%s/%s" % (filename_split[-2], filename_split[-1]),
-            'exposure_time': d['exposure_time'],
-            'timestamp': d['acquire_time'],
-            }
-        }
-        if d[u'position'] in positions:
-            positions[d[u'position']].update(plane)
-        else:
-            positions[d[u'position']] = plane
-    return positions
+def read_phenotypic_time_indexes():
+    time_indexes = {}
+    with open(MAPPINGS_FILE, 'r') as f:
+        for l in f.readlines():
+            a = l.split(' ')
+            time_indexes[a[0]] = int(a[1].rstrip()[-8:-5])
+    return time_indexes
 
 
-def write_companion(image, file_path):
+def read_phenotypic_timestamps():
+    timestamps = {}
+    with open(TIMESTAMPS_FILE, 'r') as f:
+        for l in f.readlines():
+            a = l.split(' ')
+            timestamps[a[0]] = float(a[1].rstrip())
+    return timestamps
+
+
+def parse_phenotypic_filenames(timestamps):
+    """Parse filenames of timelapse phenotypic data"""
+
+    files = {}
+    for filename in timestamps:
+        slide = filename.split('/')[0]
+        position = filename.split('/')[2]
+        files.setdefault(slide, {})
+        files[slide].setdefault(position, [])
+        files[slide][position].append(filename)
+
+    for slide in files:
+        assert len(files[slide]) == 90  # 90 positions per slide
+        for x in range(101, 146):
+            assert "Pos%s" % x in files[slide]
+        for x in range(201, 246):
+            assert "Pos%s" % x in files[slide]
+        for p in files[slide]:
+            for t in range(481):
+                filename = get_phenotypic_filename(p, "phase", t, slide=slide)
+                assert filename in files[slide][p]
+            for t in range(241):
+                filename = get_phenotypic_filename(p, "fluor", t, slide=slide)
+                assert filename in files[slide][p]
+    return files
+
+
+def get_phenotypic_filename(p, c, t, slide=None):
+    name = "%s/%s/img_000000%03g.tiff" % (p, c, t)
+    if slide is not None:
+        return "%s/pheno/%s" % (slide, name)
+    else:
+        return name
+
+
+def write_companion(images, file_path):
     # Generate companion OME-XML file
-    create_companion(images=[image], out=file_path)
+    if not os.path.exists(dirname(file_path)):
+        os.makedirs(dirname(file_path))
+        logging.info("Created %s" % dirname(file_path))
+    create_companion(images=images, out=file_path)
 
     # Indent XML for readability
     proc = subprocess.Popen(
@@ -68,50 +107,64 @@ def write_companion(image, file_path):
     logging.info("Created %s" % file_path)
 
 
-def create_phenotypic_companions(directory):
-    # Genotypic metadata generation
-    pheno_directory = join(directory, 'pheno')
-    pheno_folders = [
-        join(pheno_directory, x) for x in os.listdir(pheno_directory)]
-    pheno_folders = sorted(filter(os.path.isdir, pheno_folders))
-    logging.info(
-        "Found %g folders under %s" % (len(pheno_folders), pheno_directory))
+def create_phenotypic_companions():
+    """Phenotypic metadata files"""
 
-    positions = read_phenotypic_positions(pheno_directory)
-    for folder in pheno_folders:
-        image = Image(
-            basename(folder), 2048, 879, 1, 2, 481,
-            order="XYZCT", type="uint16")
-        image.add_channel("phase", -1)
-        image.add_channel("fluor", 16711935)
+    time_indexes = read_phenotypic_time_indexes()
+    timestamps = read_phenotypic_timestamps()
+    files = parse_phenotypic_filenames(timestamps)
+    for slide in files:
+        images = []
+        # Genotypic metadata generation
+        for position in files[slide].keys():
+            image = Image(
+                position, 2048, 879, 1, 2, 481, order="XYZCT", type="uint16")
+            image.add_channel("Phase", -1)
+            image.add_channel("Fluorescence", 16711935)
 
-        planes = positions[basename(folder)]
-        # import datetime
-        # acquisition_date = datetime.datetime.utcfromtimestamp(
-        #     planes[(0, 0)]['timestamp']/1000.0).isoformat()
+            # Phase images
+            for t in range(481):
+                relative_path = get_phenotypic_filename(position, "phase", t)
+                full_path = get_phenotypic_filename(position, "phase", t,
+                                                    slide=slide)
+                image.add_tiff(relative_path, c=0, z=0, t=t, planeCount=1)
+                plane_options = {
+                    "DeltaT": timestamps[full_path],
+                    "DeltaTUnit": "s",
+                    "ExposureTime": "20",
+                    "ExposureTimeUnit": "ms",
+                }
+                image.add_plane(c=0, z=0, t=t, options=plane_options)
 
-        for t in range(481):
-            image.add_tiff("%s/%s" % (
-                basename(folder), planes[(0, t)]["filename"]),
-                c=0, z=0, t=t)
+            # Fluorescence images
+            for i in range(241):
+                relative_path = get_phenotypic_filename(position, "fluor", i)
+                full_path = get_phenotypic_filename(position, "fluor", i,
+                                                    slide=slide)
+                t = time_indexes[full_path]
+                plane_options = {
+                    "DeltaT": timestamps[full_path],
+                    "DeltaTUnit": "s",
+                    "ExposureTime": "300",
+                    "ExposureTimeUnit": "ms",
+                }
+                if t == 2 * i:
+                    image.add_tiff(relative_path, c=1, z=0, t=t, planeCount=1)
+                    if i != 240:
+                        image.add_tiff(None, c=1, z=0, t=(2 * i) + 1)
+                elif t == (2 * i) + 1:
+                    image.add_tiff(None, c=1, z=0, t=2 * i)
+                    image.add_tiff(relative_path, c=1, z=0, t=t, planeCount=1)
+                else:
+                    raise Exception("Invalid mapping")
+                image.add_plane(c=1, z=0, t=t, options=plane_options)
+            images.append(image)
 
-        for t in range(240):
-            image.add_tiff("%s/%s" % (
-                basename(folder), planes[(1, t)]["filename"]),
-                c=1, z=0, t=2 * t)
-            image.add_tiff("%s/%s" % (
-                basename(folder), planes[(1, t)]["filename"]),
-                c=1, z=0, t=2 * t + 1)
-        image.add_tiff("%s/%s" % (
-            basename(folder), planes[(1, t)]["filename"]),
-            c=1, z=0, t=2 * t + 1)
-        image.add_tiff("%s/%s" % (
-            basename(folder), planes[(1, 240)]["filename"]),
-            c=1, z=0, t=480)
-
+        if len(images) == 0:
+            continue
         companion_file = join(
-            pheno_directory, basename(folder) + '.companion.ome')
-        write_companion(image, companion_file)
+            EXPERIMENTA_DIRECTORY, slide, "pheno", slide + '.companion.ome')
+        write_companion(images, companion_file)
 
 
 def create_genotypic_companions(directory):
@@ -162,10 +215,4 @@ def create_genotypic_companions(directory):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        'path', type=str, help='Path to the microscopic slide folder')
-    args = parser.parse_args()
-
-    create_phenotypic_companions(args.path)
-    create_genotypic_companions(args.path)
+    create_phenotypic_companions()
